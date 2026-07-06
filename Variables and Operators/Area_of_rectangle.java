fd0f42802818db0225c580dd3c2b19e9aa99a4e5
unknown <nishanthmahalingam28@gmail.com>
2026-07-06 19:16:28 +0530

    Area of recatngle

---
+++ Variables and Operators/Area_of_rectangle.java
@@ -0,0 +1,10 @@
+class Area_of_rectangle
+{
+    public static void main(String[]args)
+    {
+        double length=10.5;
+        double width=5.5;
+        double area=length*width;
+        System.out.println("Area of rectangle is :" + area);
+    }
+}
