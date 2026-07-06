22583f09e8594b9ee9785b609e28a73ec2637468
unknown <nishanthmahalingam28@gmail.com>
2026-07-06 18:48:04 +0530

    Variables and Datatype Declaration

---
+++ Variables and Operators/code1.java
@@ -0,0 +1,19 @@
+class code1
+ {
+    public static void main(String[] args)
+    {
+        //Variable declaration
+        String name="Nishanth";
+        int age=18;
+        double height =5.1;
+        char grade = 'A';
+        boolean ishostel= true;   
+        System.out.println("================================"); 
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+        System.out.println("Height:"+ height);
+        System.out.println("Grade:"+ grade);
+        System.out.println("Is Hostel:"+ ishostel);
+        System.out.println("================================");
+    }
+ }
