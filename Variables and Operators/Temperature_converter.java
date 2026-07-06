4ac14b05da1f7fe0267525d7534e210947e0c9ec
unknown <nishanthmahalingam28@gmail.com>
2026-07-06 20:23:44 +0530

    Temperatur converter

---
+++ Variables and Operators/Temperature_converter.java
@@ -0,0 +1,10 @@
+class Temperature_converter
+{
+    public static void main(String[]args)
+    {
+        double celsius=37.5;
+        double fahrenheit=(celsius*9/5)+32;
+        System.out.println("Temperature in Celsius:" + celsius);
+        System.out.println("Temperature in Fahrenheit:" + fahrenheit);
+    }
+}
