76f95e5b0bc21ecaa16acccf9e54e1ffafc63f8a
unknown <nishanthmahalingam28@gmail.com>
2026-07-06 19:10:52 +0530

    Aritmetic Calculator

---
+++ Variables and Operators/ArithMeticCalculator.java
@@ -0,0 +1,15 @@
+class ArithMeticCalculator
+ {
+     public static void main(String[] args)
+     {
+         int a=10;
+         int b=20;
+         int c= 2;
+         System.out.println("Addition of Two Numbers is :" + (a+b));
+         System.out.println("Subtraction of Two Numbers is :" + (a-b));
+         System.out.println("Multiplication of Two Numbers is :" + (a*b));
+         System.out.println("Division of Two Numbers is :" + (a/c));
+         System.out.println("Modulus of Two NUmbers is :" + (a%c));
+
+     }
+ }
